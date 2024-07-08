b9e0b9dcff3dd7468aeaa36e806d0adaad1c32a7
mikkko <danilzhmak88@gmail.com>
2024-07-08 20:37:17 +0300

    Add fixes for quantity

--- app/controllers/print_makers/orders_controller.rb
+++ app/controllers/print_makers/orders_controller.rb
@@ -7,6 +7,7 @@ module PrintMakers
     def show
       @order = Order.find(params[:id])
       @print_maker_order = PrintMakerOrder.find_or_create_by(print_maker_id: current_user.id, order_id: @order.id)
+      @available_quantity = @order.quantity - PrintMakerOrder.where(order_id: @order.id).sum(&:reserved_quantity) # maybe should be recalculated after logic with closed order will be done
     end
 
     def available_orders
